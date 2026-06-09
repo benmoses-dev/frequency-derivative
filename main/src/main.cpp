73125ac9cc36353b98bb2a91e7fcea5d7babcb4b
Ben M <ben@mosesweb.co.uk>
2026-06-09 20:54:37 +0100

    chore: Add logging and update sdk config.

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -2,11 +2,15 @@
 #include "config.h"
 #include "fft.hpp"
 #include "led.hpp"
+#include "esp_log.h"
 #include <cmath>
 
 static const char *TAG = "MAIN";
 
 extern "C" void app_main() {
+    ESP_LOGI(TAG, "Sample rate is %d", SAMPLE_RATE);
+    ESP_LOGI(TAG, "Top K average is %d", TOP_K);
+    ESP_LOGI(TAG, "FFT size is %d", BUFFER_SIZE);
     const DSP dsp(SAMPLE_RATE);
     Audio audio(dsp);
     if (!audio.init()) {
